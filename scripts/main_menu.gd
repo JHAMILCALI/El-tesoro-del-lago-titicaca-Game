ac4dd00acc3ba63021d17586925983f19313675f
JHAMILCALI <jhamilquea@gmail.com>
2026-09-07 18:50:54 -0400

    feat: implement main menu scene with background video, music, and navigation logic

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var boat_button: Button = $VBoxContainer/BoatButton
 @onready var quit_button: Button = $VBoxContainer/QuitButton
 @onready var background_video: VideoStreamPlayer = $BackgroundVideo
+@onready var background_music: AudioStreamPlayer = $BackgroundMusic
 
 var level_01_scene: PackedScene = preload("res://scenes/levels/Level01_Convoy.tscn")
 var boat_scene: PackedScene = preload("res://scenes/prototypes/BoatPrototype.tscn")
@@ -12,6 +13,11 @@ func _ready() -> void:
 	if background_video:
 		background_video.finished.connect(_on_video_finished)
 		background_video.play()
+	if background_music:
+		background_music.volume_db = linear_to_db(0.65)
+		background_music.finished.connect(_on_music_finished)
+		if not background_music.playing:
+			background_music.play()
 	if play_button:
 		play_button.pressed.connect(_on_play_pressed)
 		play_button.grab_focus()
@@ -23,6 +29,10 @@ func _ready() -> void:
 func _on_video_finished() -> void:
 	if background_video:
 		background_video.play()
+
+func _on_music_finished() -> void:
+	if background_music:
+		background_music.play()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") or event.is_action_pressed("ui_accept"):
