e38fc1d8a5cc447429af846647e55d24749e5d8f
JHAMILCALI <jhamilquea@gmail.com>
2026-09-07 15:41:26 -0400

    feat: Implementar la escena del menú principal con vídeo de fondo y botones de navegación.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,11 +3,15 @@ extends Control
 @onready var play_button: Button = $VBoxContainer/PlayButton
 @onready var boat_button: Button = $VBoxContainer/BoatButton
 @onready var quit_button: Button = $VBoxContainer/QuitButton
+@onready var background_video: VideoStreamPlayer = $BackgroundVideo
 
 var level_01_scene: PackedScene = preload("res://scenes/levels/Level01_Convoy.tscn")
 var boat_scene: PackedScene = preload("res://scenes/prototypes/BoatPrototype.tscn")
 
 func _ready() -> void:
+	if background_video:
+		background_video.finished.connect(_on_video_finished)
+		background_video.play()
 	if play_button:
 		play_button.pressed.connect(_on_play_pressed)
 		play_button.grab_focus()
@@ -15,6 +19,10 @@ func _ready() -> void:
 		boat_button.pressed.connect(_on_boat_pressed)
 	if quit_button:
 		quit_button.pressed.connect(_on_quit_pressed)
+
+func _on_video_finished() -> void:
+	if background_video:
+		background_video.play()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") or event.is_action_pressed("ui_accept"):
